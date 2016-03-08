510cc75becb22c134c22b596b4b36a043eca50c7
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2016-03-08 08:36:39 +0100

    Change cache and log directories to /var/tmp/symfony

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,4 +32,14 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        return '/var/tmp/symfony/cache/' . $this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return '/var/tmp/symfony/logs' . $this->getEnvironment();
+    }
 }
